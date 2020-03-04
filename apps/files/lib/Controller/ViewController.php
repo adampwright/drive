134cbbfad5e3f3a521821556516a67a23d7053bb
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2020-03-04 13:51:40 +0100

    Properly emit Viewer event on files and files_sharing
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

--- apps/files/lib/Controller/ViewController.php
+++ apps/files/lib/Controller/ViewController.php
@@ -39,6 +39,7 @@ namespace OCA\Files\Controller;
 use OCA\Files\Activity\Helper;
 use OCA\Files\Event\LoadAdditionalScriptsEvent;
 use OCA\Files\Event\LoadSidebar;
+use OCA\Viewer\Event\LoadViewer;
 use OCP\App\IAppManager;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http\ContentSecurityPolicy;
@@ -281,6 +282,7 @@ class ViewController extends Controller {
 		$this->eventDispatcher->dispatch(LoadAdditionalScriptsEvent::class, $event);
 
 		$this->eventDispatcher->dispatch(LoadSidebar::class, new LoadSidebar());
+		$this->eventDispatcher->dispatch(LoadViewer::class, new LoadViewer());
 
 		$params                                = [];
 		$params['usedSpacePercent']            = (int) $storageInfo['relative'];
